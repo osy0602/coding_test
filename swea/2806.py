8740b1af69c2ba04bb41907f541354dc732af69c
osy0602 <osy01036095318@gmail.com>
2024-11-15 14:17:10 +0900

    2806번 문제 풀이 DFS

---
+++ swea/2806.py
@@ -0,0 +1,25 @@
+T = int(input())
+def dfs(n):
+    global ans
+    if n ==N:
+        ans +=1
+        return
+    
+    for j in range(N):
+        if v1[j] == v2[n+j] == v3[n-j] == 0:
+            v1[j] = v2[n+j] = v3[n-j] = 1
+            dfs(n+1)
+            v1[j] = v2[n+j] = v3[n-j] = 0
+
+for tc in range(1, T+1):
+    N = int(input())
+    ans = 0
+
+    v1, v2, v3 = [[0]*(2*N) for _ in range(3)]
+
+    dfs(0)
+
+    print(f'#{tc} {ans}')
+
+    
+            
